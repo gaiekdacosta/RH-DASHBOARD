94a3e95fadccb384316e115b69d272bd287c10be
ravachebv <ravache@bvpinheiro.com.br>
2025-05-15 14:54:06 -0300

    fix: environment variables

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURLLocal = process.env.API_URL_PRODUCTION
+const baseURLLocal = import.meta.env.PUBLIC_API_URL_PRODUCTION
 
 const api = axios.create({
     baseURL: baseURLLocal
